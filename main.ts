e2cc0244ccb6e2b85be7a25a1485bcdb48d35a25
maxim-malik <maxim.malik@stu.ocsb.ca>
2025-10-26 17:18:48 -0400

    updated servo numbering

--- main.ts
+++ main.ts
@@ -9,14 +9,14 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // variables
-const servoNumber1 = robotbit.Servos.S2
+const servoNumber2 = robotbit.Servos.S2
 
 // button A input
 input.onButtonPressed(Button.A, function() {
     basic.clearScreen()
     basic.showString("0")
     // moves servo to 0 degrees
-    robotbit.Servo(servoNumber1, 0)
+    robotbit.Servo(servoNumber2, 0)
     basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
@@ -27,7 +27,7 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString("180")
     // moves servo to 180 degrees
-    robotbit.Servo(servoNumber1, 180)
+    robotbit.Servo(servoNumber2, 180)
     basic.showIcon(IconNames.SmallSquare)
     basic.clearScreen()
     basic.showIcon(IconNames.Happy)
